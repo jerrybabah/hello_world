820e180fa8c63f42e7ff045b93db9f1312f98d96
babahironobu <ghdtjs0417@gmail.com>
2020-05-09 19:31:28 +0900

    Doc: 미들웨어에 대한 정리를 주석으로 표시, app moduel뿐 아니라 cat module에도 미들웨어 붙임

--- nest/src/cat/cat.module.ts
+++ nest/src/cat/cat.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CatController } from './cat.controller';
 import { CatService } from './cat.service';
+import { logger } from './logger.middleware';
 
 @Module({
   imports: [],
@@ -8,4 +9,11 @@ import { CatService } from './cat.service';
   controllers: [CatController],
   providers: [CatService],
 })
-export class CatModule {}
+export class CatModule implements NestModule {
+  // cat module에서도 미들웨어를 적용시킬 수 있다.
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(logger)
+    .forRoutes('cats');
+  }
+}
--- nest/src/common/middlewares/logger.middleware.ts
+++ nest/src/common/middlewares/logger.middleware.ts
@@ -1,6 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+/**
+ * nest에서 미들웨어도 provider의 일종이므로 Injectable 데코레이터를 붙인 클래스로 정의한다. (함수로 정의해도 됨)
+ * 클래스로 정의할 경우 추가로 NestMiddleware라는 interface를 구현해야 한다. (함수는 추가로 해야 할 것 없음)
+ * 
+ * provider를 DI시키는 방법으로 Module 데코레이터의 option 인자에 넣을 수 있지만 미들웨어는 다른 방법으로 적용한다.
+ * module 클래스에 NestModule interface를 구현시키고 configure라는 메소드를 구현함으로써 미들웨어를 적용한다.
+ */
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
