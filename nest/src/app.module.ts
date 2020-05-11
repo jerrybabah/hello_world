d92f277daa7d3d4c2a435584e20bb4abfb97afaa
babahironobu <ghdtjs0417@gmail.com>
2020-05-11 22:43:48 +0900

    Chor: 간단한 미들웨어 적용 범위 실험

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -9,6 +9,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes(''); // 모든 요청에서 logger를 띄운다.
+      .forRoutes(''); // 모든 요청에서 logger를 띄운다. express에서처럼 라우트 앞에 미들웨어를 붙이는 것과 같다.
   }
 }
